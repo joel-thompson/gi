851142ec9718064260f86d910d2ac16bd53596be
Joel Thompson <joel.thompson@circleci.com>
2025-05-19 12:33:03 -0700

    refactor: improve app.tsx structure and readability

--- source/app.tsx
+++ source/app.tsx
@@ -9,6 +9,7 @@ import Spinner from "ink-spinner";
 
 type Props = {
 	commit: boolean | undefined;
+	dryRun: boolean | undefined;
 };
 
 // const options: Partial<SimpleGitOptions> = {
@@ -20,7 +21,7 @@ type Props = {
 
 // when setting all options in a single object
 
-export default function App({ commit = false }: Props) {
+export default function App({ commit = false, dryRun = false }: Props) {
 	const [aiResponse, setAIResponse] = useState<string | null>(null);
 	useEffect(() => {
 		if (commit) {
@@ -31,7 +32,7 @@ export default function App({ commit = false }: Props) {
 				const { text } = await generateText({
 					model: openai.responses("gpt-4o-mini"),
 					system:
-						"You are an expert at generating commit messages. Please provide a commit message for the changes in the diff. It should be a single line of text, no more than 50 characters. IMPORTANT: ignore changes to lock files, node_modules, and other files that are not part of the project.",
+						"You are an expert at generating commit messages. Please provide a commit message for the changes in the diff. It should be a single line of text, no more than 50 characters. IMPORTANT: ignore changes to lock files, node_modules, and other files that are not part of the project. IMPORTANT: the commit message should be in the present tense. IMPORTANT: use Conventional Commits, example: feat: add new feature, fix: fix bug, chore: update dependencies, refactor: refactor code, style: style changes, perf: performance improvements, test: add tests, docs: update documentation, build: update build files, ci: update CI files, revert: revert changes, merge: merge changes, chore: other changes that don't fit into the other categories.",
 					prompt: `
 					Here is the diffs of the repository:
 					${JSON.stringify(wholeRepoStatus, null, 2)}
@@ -39,7 +40,9 @@ export default function App({ commit = false }: Props) {
 				});
 				setAIResponse(text);
 
-				await git.add("./*").commit(text);
+				if (!dryRun) {
+					await git.add("./*").commit(text);
+				}
 			};
 			fetchStatus();
 		}
